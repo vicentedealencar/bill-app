a8d005e09d6219eacdf72a690e6e855776ba404f
victor hugo <vicentedealencar@gmail.com>
2017-10-19 17:46:12 -0200

    played on android

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default class App extends React.Component {
   render() {
@@ -8,9 +8,14 @@ export default class App extends React.Component {
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        <Button rounded onPress={onBuy} title="paaaay" />
       </View>
     );
   }
+}
+
+const onBuy = () => {
+  console.log("paaaaaaaaaay")
 }
 
 const styles = StyleSheet.create({
--- android/app/src/main/java/com/br/billapp/generated/ExponentBuildConstants.java
+++ android/app/src/main/java/com/br/billapp/generated/ExponentBuildConstants.java
@@ -3,6 +3,6 @@ package com.br.billapp.generated;
 // This file is auto-generated. Please don't rename!
 public class ExponentBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "";
+  public static final String DEVELOPMENT_URL = "expf2a6010e51f8424aaf95f8152c8aaf3a://cd-a4f.vicente.bill-app.exp.direct:80";
 
 }
