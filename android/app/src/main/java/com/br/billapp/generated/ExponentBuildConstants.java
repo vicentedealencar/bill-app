c7acfa7b4d7d10262f299575a39cfe502b351141
victor hugo <vicentedealencar@gmail.com>
2017-10-19 17:27:21 -0200

    eject

---
+++ android/app/src/main/java/com/br/billapp/generated/ExponentBuildConstants.java
@@ -0,0 +1,8 @@
+package com.br.billapp.generated;
+
+// This file is auto-generated. Please don't rename!
+public class ExponentBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "";
+
+}
